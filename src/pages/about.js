5787ea7a95d146638093d75f67fb84389a6b7de5
Gwen Zhou <gwen.zhou@servelec.co.uk>
2020-12-11 08:30:12 +0000

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+        <Link to="/contact/">Contact</Link>
+
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Home() {
+  return (
+  <div style={{ color: `purple`}}>
+    <h1>Hello Gwen!</h1>
+    <p>pppppp</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+  )
+}
